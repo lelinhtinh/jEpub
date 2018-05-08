f03c991cfa0113e48c135d6a5fe6b71248063949
baivong <Zzbaivong@gmail.com>
2018-05-08 11:12:04 +0700

    Add Browser Sync

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -1,5 +1,6 @@
 /* eslint-env node */
 const path = require('path');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -11,5 +12,16 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'jepub.js'
-    }
+    },
+    devtool: 'source-map',
+    plugins: [
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 3333,
+            server: {
+                baseDir: ['demo', 'dist']
+            },
+            files: ['demo', 'dist']
+        })
+    ]
 };
