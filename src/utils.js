9f6e285f77cde767e47facaa848a35086cf43b36
baivong <Zzbaivong@gmail.com>
2018-05-13 07:02:27 +0700

    Add parseDOM, html2text to utils

--- src/utils.js
+++ src/utils.js
@@ -34,9 +34,42 @@ export function isEmpty(val) {
 }
 
 /**
- * get current moment in ISO format
- * @returns {string} date
+ * Get current moment in ISO format
+ * @param {Object} date
+ * @returns {string} ISO date
  */
-export function getCurrentDate() {
-    return (new Date).toISOString();
+export function getCurrentDate(date = new Date()) {
+    return date.toISOString();
+}
+
+/**
+ * Convert convert HTML to valid XHTML
+ * @param {String} html
+ * @param {String} outText return as plain text
+ */
+export function parseDOM(html, outText = false) {
+    let doc = (new DOMParser).parseFromString(
+        `<!doctype html><body>${html}`,
+        'text/html');
+    if (outText) return doc.body.textContent;
+    doc = (new XMLSerializer).serializeToString(doc.body);
+    doc = doc.replace(/(^<body\s?[^>]*>|<\/body>$)/g, '');
+    return doc;
+}
+
+/**
+ * Convert HTML to plain text
+ * @param {String} html
+ */
+export function html2text(html, noBr = false) {
+    html = html.replace(/<style([\s\S]*?)<\/style>/gi, '');
+    html = html.replace(/<script([\s\S]*?)<\/script>/gi, '');
+    html = html.replace(/<\/(div|p|li|dd|h[1-6])>/gi, '\n');
+    html = html.replace(/<(br|hr)\s*[/]?>/gi, '\n');
+    html = html.replace(/<li>/ig, '+ ');
+    html = html.replace(/<[^>]+>/g, '');
+    html = html.replace(/\n{3,}/g, '\n\n');
+    html = parseDOM(html, true);
+    if (noBr) html = html.replace(/\n+/g, ' ');
+    return html;
 }
