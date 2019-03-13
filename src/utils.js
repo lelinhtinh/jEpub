76e22149d37ee8ee2242ad0c0cb26c6c7eca11d5
baivong <Zzbaivong@gmail.com>
2019-03-13 22:56:36 +0700

    Convert MIME type to extension

--- src/utils.js
+++ src/utils.js
@@ -5,7 +5,7 @@
  */
 
 export function uuidv4() {
-    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
         (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
     );
 }
@@ -72,4 +72,30 @@ export function html2text(html, noBr = false) {
  */
 export function validateUrl(value) {
     return /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value);
+}
+
+/**
+ * Convert MIME type to extension
+ * @param {String} mime
+ */
+export function mime2ext(mime) {
+    let ext = null;
+    switch (mime) {
+    case 'image/gif':
+        ext = 'gif';
+        break;
+    case 'image/jpeg':
+        ext = 'jpg';
+        break;
+    case 'image/png':
+        ext = 'png';
+        break;
+    case 'image/svg+xml':
+        ext = 'svg';
+        break;
+    default:
+        ext = null;
+        break;
+    }
+    return ext;
 }
