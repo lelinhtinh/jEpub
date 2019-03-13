6a2d7df8d306f7b72d991dc88799cd0ba56f41ce
baivong <Zzbaivong@gmail.com>
2019-03-13 23:11:29 +0700

    Update demo

--- demo/script.js
+++ demo/script.js
@@ -1,10 +1,13 @@
 /* eslint-disable no-console */
-fetch('./cover-image.jpg').then(response => {
-    if (response.ok) return response.arrayBuffer();
-    throw 'Network response was not ok.';
-}).then(buffer => {
+(async () => {
 
-    const jepub = new jEpub({
+    const cover = await fetch('./cover-image.jpg').then(response => {
+        if (response.ok) return response.blob();
+        throw 'Network response was not ok.';
+    });
+
+    const jepub = new jEpub();
+    jepub.init({
         title: 'Lorem Ipsum',
         author: 'Cicero',
         publisher: 'lipsum.com',
@@ -14,14 +17,20 @@ fetch('./cover-image.jpg').then(response => {
 
     jepub.uuid('https://vi.lipsum.com/');
     jepub.date(new Date(2013, 8, 15));
+    jepub.cover(cover).notes('<p class="no-indent">Dịch sang Tiếng Việt bởi <strong>Đỗ Việt</strong> - www.doviet.net</p>');
 
-    jepub.cover(buffer).notes('<p class="no-indent">Dịch sang Tiếng Việt bởi <strong>Đỗ Việt</strong> - www.doviet.net</p>');
+    let illustration = await fetch('./lorem-ipsum.jpg').then(response => {
+        if (response.ok) return response.blob();
+        throw 'Network response was not ok.';
+    });
+    jepub.image(illustration, 'lorem-ipsum');
 
-    jepub.add('Lorem Ipsum là gì?', [
-        'Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những đã tồn tại năm thế kỉ, mà khi được áp dụng vào tin học văn phòng, nội dung của nó vẫn không hề bị thay đổi. Nó đã được phổ biến trong những năm 1960 nhờ việc bán những bản giấy Letraset in những đoạn Lorem Ipsum, và gần đây hơn, được sử dụng trong các ứng dụng dàn trang, như Aldus PageMaker.',
-        'Trái với quan điểm chung của số đông, Lorem Ipsum không phải chỉ là một đoạn văn bản ngẫu nhiên. Người ta tìm thấy nguồn gốc của nó từ những tác phẩm văn học la-tinh cổ điển xuất hiện từ năm 45 trước Công Nguyên, nghĩa là nó đã có khoảng hơn 2000 tuổi. Một giáo sư của trường Hampden-Sydney College (bang Virginia - Mỹ) quan tâm tới một trong những từ la-tinh khó hiểu nhất, "consectetur", trích từ một đoạn của Lorem Ipsum, và đã nghiên cứu tất cả các ứng dụng của từ này trong văn học cổ điển, để từ đó tìm ra nguồn gốc không thể chối cãi của Lorem Ipsum. Thật ra, nó được tìm thấy trong các đoạn 1.10.32 và 1.10.33 của "De Finibus Bonorum et Malorum" (Đỉnh tối thượng của Cái Tốt và Cái Xấu) viết bởi Cicero vào năm 45 trước Công Nguyên. Cuốn sách này là một luận thuyết đạo lí rất phổ biến trong thời kì Phục Hưng. Dòng đầu tiên của Lorem Ipsum, "Lorem ipsum dolor sit amet..." được trích từ một câu trong đoạn thứ 1.10.32.',
-        'Trích đoạn chuẩn của Lorem Ipsum được sử dụng từ thế kỉ thứ 16 và được tái bản sau đó cho những người quan tâm đến nó. Đoạn 1.10.32 và 1.10.33 trong cuốn "De Finibus Bonorum et Malorum" của Cicero cũng được tái bản lại theo đúng cấu trúc gốc, kèm theo phiên bản tiếng Anh được dịch bởi H. Rackham vào năm 1914.'
-    ]);
+    jepub.add('Lorem Ipsum là gì?', `
+        <p class="indent">Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những đã tồn tại năm thế kỉ, mà khi được áp dụng vào tin học văn phòng, nội dung của nó vẫn không hề bị thay đổi. Nó đã được phổ biến trong những năm 1960 nhờ việc bán những bản giấy Letraset in những đoạn Lorem Ipsum, và gần đây hơn, được sử dụng trong các ứng dụng dàn trang, như Aldus PageMaker.</p>
+        <p style="text-align:center;"><%= image['lorem-ipsum'] %></p>
+        <p class="indent">Trái với quan điểm chung của số đông, Lorem Ipsum không phải chỉ là một đoạn văn bản ngẫu nhiên. Người ta tìm thấy nguồn gốc của nó từ những tác phẩm văn học la-tinh cổ điển xuất hiện từ năm 45 trước Công Nguyên, nghĩa là nó đã có khoảng hơn 2000 tuổi. Một giáo sư của trường Hampden-Sydney College (bang Virginia - Mỹ) quan tâm tới một trong những từ la-tinh khó hiểu nhất, "consectetur", trích từ một đoạn của Lorem Ipsum, và đã nghiên cứu tất cả các ứng dụng của từ này trong văn học cổ điển, để từ đó tìm ra nguồn gốc không thể chối cãi của Lorem Ipsum. Thật ra, nó được tìm thấy trong các đoạn 1.10.32 và 1.10.33 của "De Finibus Bonorum et Malorum" (Đỉnh tối thượng của Cái Tốt và Cái Xấu) viết bởi Cicero vào năm 45 trước Công Nguyên. Cuốn sách này là một luận thuyết đạo lí rất phổ biến trong thời kì Phục Hưng. Dòng đầu tiên của Lorem Ipsum, "Lorem ipsum dolor sit amet..." được trích từ một câu trong đoạn thứ 1.10.32.</p>
+        <p class="indent">Trích đoạn chuẩn của Lorem Ipsum được sử dụng từ thế kỉ thứ 16 và được tái bản sau đó cho những người quan tâm đến nó. Đoạn 1.10.32 và 1.10.33 trong cuốn "De Finibus Bonorum et Malorum" của Cicero cũng được tái bản lại theo đúng cấu trúc gốc, kèm theo phiên bản tiếng Anh được dịch bởi H. Rackham vào năm 1914.</p>
+    `);
 
     jepub.add('Tại sao lại sử dụng nó?', '<p class="indent">Chúng ta vẫn biết rằng, làm việc với một đoạn văn bản dễ đọc và rõ nghĩa dễ gây rối trí và cản trở việc tập trung vào yếu tố trình bày văn bản. Lorem Ipsum có ưu điểm hơn so với đoạn văn bản chỉ gồm nội dung kiểu "Nội dung, nội dung, nội dung" là nó khiến văn bản giống thật hơn, bình thường hơn. Nhiều phần mềm thiết kế giao diện web và dàn trang ngày nay đã sử dụng Lorem Ipsum làm đoạn văn bản giả, và nếu bạn thử tìm các đoạn "Lorem ipsum" trên mạng thì sẽ khám phá ra nhiều trang web hiện vẫn đang trong quá trình xây dựng. Có nhiều phiên bản khác nhau đã xuất hiện, đôi khi do vô tình, nhiều khi do cố ý (xen thêm vào những câu hài hước hay thông tục).</p>');
 
@@ -30,21 +39,15 @@ fetch('./cover-image.jpg').then(response => {
     ]);
 
     console.log(jepub);
+    const blob = await jepub.generate();
+    console.log(blob);
 
-    jepub.generate().then(content => {
-        console.log(content);
+    let url = URL.createObjectURL(blob);
+    let link = document.createElement('a');
 
-        let url = URL.createObjectURL(content);
+    document.body.appendChild(link);
+    link.href = url;
+    link.textContent = 'Download EPUB';
+    link.download = 'lorem-ipsum.epub';
 
-        let link = document.createElement('a');
-        document.body.appendChild(link);
-        link.href = url;
-        link.textContent = 'Download EPUB';
-        link.download = 'lorem-ipsum.epub';
-    }).catch(err => {
-        console.error(err);
-    });
-
-}).catch(err => {
-    console.error(err);
-});
+})();
