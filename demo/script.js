b9406a515fda792065ed66c1141ae726fbd2e438
lelinhtinh <lelinhtinh2013@gmail.com>
2020-06-21 16:48:36 +0700

    Add onUpdate callback

--- demo/script.js
+++ demo/script.js
@@ -42,7 +42,10 @@
     ]);
 
     console.log(jepub);
-    const blob = await jepub.generate();
+    const blob = await jepub.generate('blob', metadata => {
+        console.log('progression: ' + metadata.percent.toFixed(2) + ' %');
+        if (metadata.currentFile) console.log('current file = ' + metadata.currentFile);
+    });
     console.log(blob);
 
     let url = URL.createObjectURL(blob);
